47905ac7ff36b78e407e666929b9719cd7b7b544
pengyw97 <877285030@qq.com>
2022-02-21 17:29:58 +0800

    fix: 修复由于APP.vue内缺少router-view的bug

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <Index />
+  <router-view />
 </template>
 
 <script>
 
-import Index from './pages/Index'
-
 export default {
   name: 'App',
-  components: {
-    Index
-  }
 }
 </script>
 
