d7eb44010fee53c82bfda6a19fab016319a2d611
pengyw97 <877285030@qq.com>
2022-02-24 16:25:17 +0800

    feat: 轮播图左侧导航栏

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <div class="container">
       <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item" v-for="(m, i) in menuList" :key="i">
+              <span>{{ m.name }}</span>
+              <!-- <div class="children" v-if="m.children">111</div> -->
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiperSlide v-for="p in slideList" :key="p.id">
             <router-link :to="`/product/${p.id}`"><img :src="p.img"></router-link>
@@ -69,18 +77,94 @@ export default {
           img:'/imgs/slider/slide-1.jpg'
         }
       ],
+      menuList: [
+        {
+          name: '手机 电话卡',
+          children: {},
+        },
+        {
+          name: '电视 盒子',
+          children: null,
+        },
+        {
+          name: '笔记本 平板',
+          children: null,
+        },
+        {
+          name: '家电 插线板',
+          children: null,
+        },
+        {
+          name: '出行 穿戴',
+          children: null,
+        },
+        {
+          name: '智能 路由器',
+          children: null,
+        },
+        {
+          name: '电源 配件',
+          children: null,
+        },
+        {
+          name: '生活 箱包',
+          children: null,
+        },
+      ],
     }
   }
 }
 </script>
 <style lang='scss' scoped>
+  @import '../assets/scss/config.scss';
+  @import '../assets/scss/mixin.scss';
+
   .home {
+
     .swiper-box {
-    .swiper-container {
+      position: relative;
+
+      .nav-menu {
+        box-sizing: border-box;
+        width: 264px;
         height: 451px;
+        position: absolute;
+        z-index: 9;
+        padding: 26px 0;
+        background-color: #55585A7A;
+
+        .menu-wrap {
+          .menu-item {
+            height: 50px;
+            line-height: 50px;
+            font-size: $fontI;
+            color: $colorG;
+            padding-left: 30px;
+            position: relative;
+
+            &:after {
+              content: '';
+              @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+            }
+
+            &:hover {
+              background-color: $colorA;
+              cursor: pointer;
+            }
+          }
+        }
+      }
+
+      .swiper-container {
+        height: 451px;
+
         .swiper-button-prev{
           left:274px;
         }
+
         img{
           width:100%;
           height:100%;
