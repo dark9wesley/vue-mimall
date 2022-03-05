adf19cf8615e243f6b95ea581c2fa9a0beb6b8af
pengyw97 <877285030@qq.com>
2022-03-06 01:45:40 +0800

    feat: orderHeader component

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,13 +1,62 @@
 <template>
-  <div>
-    
+  <div class="order-header">
+    <div class="container clearfix">
+      <div class="header-logo fl">
+        <router-link to="/"></router-link>
+      </div>
+      <div class="title fl">
+        <h2>{{ title }}<span>{{ tips }}</span></h2>
+      </div>
+      <div class="username fr">
+        <a href="javascript:;">{{ username }}</a>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  
+  name: 'OrderHeader',
+  props: ['title', 'tips'],
+  computed: {
+    ...mapState(['username'])
+  }
 }
 </script>
-<style lang="">
-  
+<style lang="scss" scoped>
+  @import '../assets/scss/config.scss';
+
+  .order-header {
+    padding: 30px 0;
+    border-bottom: 2px solid $colorA;
+
+    .title, .username {
+      display: inline-block;
+      height: 55px;
+      line-height: 55px;
+    }
+
+    .title {
+      margin-left:54px;
+
+      h2 {
+        font-size: $fontC;
+        color: $colorB;
+      }
+
+      span {
+        font-size: $fontJ;
+        margin-left: 17px;
+        color: $colorD;
+        font-weight: 200;
+      }
+    }
+    .username {
+      a {
+        color: $colorC;
+        font-size: $fontI;
+      }
+    }
+  }
 </style>
--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
+  <div class="cart">
+    <OrderHeader title="我的购物车" tips="温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算" />
     Cart
+    <NavFooter />
   </div>
 </template>
 <script>
+import OrderHeader from '../components/OrderHeader'
+import NavFooter from '../components/NavFooter'
+
 export default {
-  name: 'Cart'
+  name: 'Cart',
+  components: {
+    OrderHeader, 
+    NavFooter,
+  }
 }
 </script>
 <style lang="scss">
